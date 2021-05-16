deaf534fd00ff343ef62ee18cb1b627b4cdb7469
Daniel <wendaniel@hotmail.com>
2021-05-16 00:46:50 -0400

    initital commit

--- src/main/java/org/atwjsw/HelloServlet.java
+++ src/main/java/org/atwjsw/HelloServlet.java
@@ -12,6 +12,6 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("hello maven world");
+        resp.getWriter().println("hello maven world XXX");
     }
 }
