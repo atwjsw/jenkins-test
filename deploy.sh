25510dae799b2579e8d0d2adfbf7c04963c001da
Daniel <wendaniel@hotmail.com>
2021-05-16 00:21:28 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 cd $PROJ_PATH/order
 mvn clean install
 
-cp $PROJ_PATH/order/target/trans08.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/order/target/trans08.war $TOMCAT_APP_PATH/
 
-cd $TOMCAT_APP_PATH/webapps/
+cd $TOMCAT_APP_PATH
 mv trans08.war ROOT.war
 
 cd /usr/share/tomcat7
