a9eace468b045675d601c5969275acbb11ce6df3
Daniel <wendaniel@hotmail.com>
2021-05-16 00:18:49 -0400

    initital commit

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 cd $PROJ_PATH/order
 mvn clean install
+
+cp $PROJ_PATH/order/target/trans08.war $TOMCAT_APP_PATH/webapps/
+
+cd $TOMCAT_APP_PATH/webapps/
+mv trans08.war ROOT.war
+
+cd /usr/share/tomcat7
+sh bin/startup.sh
